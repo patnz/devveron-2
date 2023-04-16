906fb16cf7b2a5f333009bcd361ae98f6be53fbe
SamMiddleton <sam@fishstat.com>
2023-04-17 09:15:43 +1200

    WIP stuff

--- client/components/Morris.tsx
+++ client/components/Morris.tsx
@@ -10,6 +10,8 @@ interface Tile {
   cb: null | (() => void)
 }
 
+type TurnState = 'OPPONENTS' | 'SELECT PIECE' | 'PLACE PIECE' | 'CAPTURE PIECE'
+
 export default function Morris({ socket }: Props) {
   const [board, setBoard] = useState(
     Array.from({ length: 3 }, () =>
@@ -17,9 +19,48 @@ export default function Morris({ socket }: Props) {
     )
   )
   const [colour, setColour] = useState('⚪' as '⚪' | '⚫')
-  const [myTurn, setMyTurn] = useState(false)
+  const [turnState, setTurnState] = useState('OPPONENTS' as TurnState)
   const [phase, setPhase] = useState(0)
   const [pieces, setPieces] = useState(9)
+  const [activePiece, setActivePiece] = useState(
+    undefined as undefined | [number, number]
+  )
+
+  switch (turnState) {
+    case 'SELECT PIECE':
+      board.forEach((ring, i) =>
+        ring.forEach((tile, j) => {
+          if (tile.p === colour) {
+            if (
+              phase === 3 ||
+              ring[(j + 1) % 8].p === '' ||
+              ring[(j + 7) % 8].p === '' ||
+              (j % 2 === 1 &&
+                ((i > 0 && board[i - 1][j].p === '') ||
+                  (i < 2 && board[i + 1][j].p === '')))
+            ) {
+              tile.cb = () => {
+                setActivePiece([i, j])
+                setTurnState('PLACE PIECE')
+              }
+            }
+          }
+        })
+      )
+      break
+    case 'PLACE PIECE':
+      if (phase === 2) {
+      } else {
+        board.forEach((ring, i) =>
+          ring.forEach((tile, j) => {
+            if (tile.p === '') {
+              if (activePiece) {
+              }
+            }
+          })
+        )
+      }
+  }
 
   return (
     <div className="morris">
@@ -37,3 +78,9 @@ export default function Morris({ socket }: Props) {
     </div>
   )
 }
+
+function swap(
+  board: Tile[][],
+  [i0, j0]: [number, number],
+  [i1, j1]: [number, number]
+): Tile[][] {}
