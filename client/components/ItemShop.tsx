1d88ec1f055a66a08f374e4f1bfe262aacab3ed3
Sacha Beaumier <sacha.beaumier@gmail.com>
2023-04-19 09:35:28 +1200

    stashing changes

--- client/components/ItemShop.tsx
+++ client/components/ItemShop.tsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom'
 interface Props {
   player: Player
   setPlayer: (player: Player) => void
+  addGold: (gold: number) => void
+  addItems: (item: string) => void
 }
 
-function ItemShop({ player, setPlayer }: Props) {
+function ItemShop({ player, setPlayer, addGold, addItems }: Props) {
   return (
     <>
       <div className="location-name">
@@ -14,6 +16,9 @@ function ItemShop({ player, setPlayer }: Props) {
       </div>
       <div className="location-content-container">
         <p>
+          Welcome to the Redux Store! We GET and SEND things from our warehouse
+          and wait for things to get to us through our teams of ACTIONS and
+          REDUCERS so that you do not have to wait!
           <Link to="/loc/town-square" className="link">
             Town Square
           </Link>
