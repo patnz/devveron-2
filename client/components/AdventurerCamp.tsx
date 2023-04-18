b943c153e80efbe91f990ef06d38f944bce9db91
gerard-verhaart <gverhaart@gmail.com>
2023-04-19 10:25:47 +1200

    added content to adventurer camp

--- client/components/AdventurerCamp.tsx
+++ client/components/AdventurerCamp.tsx
@@ -14,10 +14,20 @@ function AdventurerCamp({ player, setPlayer }: Props) {
       </div>
       <div className="location-content-container">
         <p>
+          You enter a merry little camp full of eccentric rogues and
+          adventurers. There is a mighty bonfire in the middle, and there are
+          pavillions and cooking fires scattered around. You can hear the sounds
+          of laughter, chatter, and some light brawling. Behind you is the{' '}
           <Link to="/loc/town-entrance" className="link">
             Town Entrance
           </Link>
           .
+        </p>
+        <p>
+          One of the adventurers approaches you. &ldquo;Hello! My name is Yosan
+          the Rogue, welcome to my camp - we have food, drink, and fun! We help
+          out the local townspeople with quests and you can trade and gamble
+          with us as well.&rdquo;
         </p>
       </div>
     </>
