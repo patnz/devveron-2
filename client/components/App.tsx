6a33079737826abbd50c2ada95722940cf20ef27
gerard-verhaart <gverhaart@gmail.com>
2023-04-19 14:33:59 +1200

    set up castle ternery

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -224,7 +224,7 @@ function App() {
                 <Castle
                   player={fullPlayer}
                   addGold={addGold}
-                  updateQuests={updateQuests}
+                  updateEvents={updateEvents}
                   removeItems={removeItems}
                 />
               }
