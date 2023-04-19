6a55c603458d7b57ec96afda0de35f9e06ab2d82
Pat Wilson <pat@wilsons.org.nz>
2023-04-19 14:26:16 +1200

    yes

--- client/components/Docks.tsx
+++ client/components/Docks.tsx
@@ -23,39 +23,113 @@ function Docks({
     updateEvents({ gaveFalcon: true })
   }, [])
 
+  const [contentStage, setContentStage] = useState(0)
+  // player.progress.events.metPat ? 3 : 0
+
+  const handleClickNext = () => {
+    if (contentStage < 3) {
+      // if (contentStage === 2) {
+      //   updateEvents({ metPat: true }) /// optional <------
+      // }
+      setContentStage(contentStage + 1)
+    }
+  }
+  const handleClickBack = () => {
+    if (contentStage > 0) {
+      setContentStage(contentStage - 1)
+    }
+  }
   return (
     <>
       <div className="location-name">
         <h2>The Docks of Devveron</h2>
       </div>
       <div className="location-content-container">
-        <p>
-          You arrive at the Docks and are struck by the deep blue of the sea,
-          and the freshness of the ocean air. The seagulls glide and squawk
-          overhead and the sailors are milling about their Ships with fine ales
-          and card games. Behind you is the road leading back to the{' '}
-          <Link to="/loc/town-square" className="link">
-            Town Square
-          </Link>
-          . At the corner of your eye you spy someone that looks authoritative
-          but friendly! Mayor Kelly: &ldquo;Hello Traveller and welcome to the
-          Docks! The route to get here must have been easy to GET to. We are
-          querying with contractors about building a newer route here so that
-          the people of Devveron can get things from the dock a lot more faster
-          and efficiently. By chance, were you sent here by Pat the Street
-          Urchin? I am wondering whether you have an item I had stashed at
-          Pat&apos;s, the mythical Lego set of the Millenium Falcon?&rdquo;
-        </p>
-        {player.progress.events.gaveFalcon ? (
-          <p>You have found Kelly!</p>
-        ) : (
-          <button onClick={giveFalcon}>
-            Give the Millenium Falcon Lego Set to Kelly?
-          </button>
+        {contentStage === 0 && (
+          <div className="location-content typerwriter">
+            <p className="docks-content-1 typewriter">
+              You arrive at the Docks and are struck by the deep blue of the
+              sea, and the freshness of the ocean air. The seagulls glide and
+              squawk overhead and the sailors are milling about their Ships with
+              fine ales and card games. Behind you is the road leading back to
+              the{' '}
+              <Link to="/loc/town-square" className="link">
+                Town Square
+              </Link>
+              .
+            </p>
+            <p className="docks-content-2 typewriter">
+              At the corner of your eye you spy someone that looks authoritative
+              but friendly!
+            </p>
+
+            <button className="next-content-button" onClick={handleClickNext}>
+              ⮕
+            </button>
+          </div>
+        )}
+        {contentStage === 1 && (
+          <div className="location-content typerwriter">
+            <p className="player-quote">Mayor Kelly:</p>
+
+            <p className="npc-quote">
+              <span className="quote-icon">➶ </span>"Hello Traveller and welcome
+              to the Docks! The route to get here must have been easy to GET to.
+              We are querying with contractors about building a newer route here
+              so that the people of Devveron can get things from the dock a lot
+              more faster and efficiently."
+            </p>
+            <p className="npc-quote">
+              <span className="quote-icon">➶ </span> "By chance, were you sent
+              here by Pat the Street Urchin? I am wondering whether you have an
+              item I had stashed at Pat's, the mythical Lego set of the
+              Millenium Falcon?..”
+            </p>
+            <button className="back-content-button" onClick={handleClickBack}>
+              ⬅
+            </button>
+            <button className="next-content-button" onClick={handleClickNext}>
+              ⮕
+            </button>
+          </div>
+        )}
+
+        {contentStage === 2 && (
+          <div className="location-content">
+            {player.progress.events.gaveFalcon ? (
+              <>
+                <p>You have found Kelly!</p>
+                <button
+                  className="back-content-button"
+                  onClick={handleClickBack}
+                >
+                  ⬅
+                </button>
+              </>
+            ) : (
+              <>
+                <p>
+                  Kelly stares longingly at the Millenium Falcon Lego Set
+                  perking out the top of your rucksack...
+                </p>
+                <button className="action-text-button" onClick={giveFalcon}>
+                  Hand over Lego
+                </button>
+                <button
+                  className="back-content-button"
+                  onClick={handleClickBack}
+                >
+                  ⬅
+                </button>
+              </>
+            )}
+          </div>
         )}
       </div>
     </>
   )
+
+  return <></>
 }
 
 export default Docks
